945c0faae7fe83ca76173b1b8d22343658bdc5d4
Fariba <fariba@b01-aruba1-guest-10-110-72-109.wlan.cpp.edu>
2017-01-18 18:44:56 -0800

    first

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+<!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML>
+   <HEAD>
+      <TITLE>
+         A Small Hello 
+      </TITLE>
+   </HEAD>
+<BODY>
+   <H1>Hi</H1>
+   <P>My name is Fariba. I am majoring in Computer Science.</P> 
+</BODY>
+</HTML>
